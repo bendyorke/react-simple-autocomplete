aa5d392ea26871630834a5eba164e06589052866
Ben Yorke <bendyorke@gmail.com>
2016-01-20 23:54:04 -0500

    Initial commit

---
+++ server.dev.js
@@ -0,0 +1,27 @@
+var path = require('path')
+var express = require('express')
+var webpack = require('webpack')
+var config = require('./webpack.config.dev')
+
+var app = express()
+var compiler = webpack(config)
+
+var HOST = process.env.HOST || '0.0.0.0'
+var PORT = process.env.PORT || 3300
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+
+app.use(require('webpack-hot-middleware')(compiler))
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dev.html'))
+})
+
+app.listen(PORT, HOST, function(err) {
+  if (err) throw err
+
+  console.log(`Listening at http://${HOST}:${PORT}`)
+})
