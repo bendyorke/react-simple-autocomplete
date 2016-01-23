80ea55392ff7c8e2085e41003939d153992f0226
Ben Yorke <bendyorke@gmail.com>
2016-01-22 21:34:27 -0500

    prod -> production

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    filename: '[name].dev.bundle.js',
     publicPath: '/static/',
   },
   plugins: [
